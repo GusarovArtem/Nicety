3d3ebba04bb6f78841669edf34ca5271b75544f3
GusarovArtem <artemmaksimovich1@gmail.com>
2022-06-02 08:04:04 +0300

    Initial commit

---
+++ src/test/java/ua/nicety/NicetyApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.nicety;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NicetyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
