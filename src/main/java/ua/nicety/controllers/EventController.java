ec8fd05931a40166fe54cc70ad3d39c9e6f6aca8
GusarovArtem <artemmaksimovich1@gmail.com>
2022-06-16 11:02:29 +0300

    Add mappings for controllers

--- src/main/java/ua/nicety/controllers/EventController.java
+++ src/main/java/ua/nicety/controllers/EventController.java
@@ -1,9 +1,76 @@
 package ua.nicety.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import ua.nicety.DAO.EventDAO;
+import ua.nicety.model.Event;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/events")
 public class EventController {
+
+    private final EventDAO eventDAO;
+
+    @Autowired
+    public EventController(EventDAO eventDAO) {
+        this.eventDAO = eventDAO;
+    }
+
+    //  Show all schedule events
+    @GetMapping("/{id}")
+    public String userSchedules(
+            @PathVariable("id") Event event,
+            Model model) {
+        model.addAttribute("user", eventDAO.allScheduleEvents(event.getScheduleId()));
+
+        return "events/show";
+    }
+
+    //  Create new Schedule
+    @GetMapping("/new")
+    public String newSchedule(Model model, Event event) {
+        model.addAttribute("event", event);
+
+        return "events/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("event") @Valid Event event,
+                         BindingResult bindingResult) {
+        if (bindingResult.hasErrors())
+            return "events/new";
+
+        eventDAO.save(event);
+        return "redirect:/events";
+    }
+
+    //  Edit & update schedule
+    @GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable("id") Long id) {
+        model.addAttribute("event", eventDAO.show(id));
+        return "event/edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("schedule") @Valid Event event,
+                         BindingResult bindingResult,
+                         @PathVariable("id") Long id) {
+        if (bindingResult.hasErrors())
+            return "events/edit";
+
+        eventDAO.update(id, event);
+        return "redirect:/events";
+    }
+
+    //  Delete schedule
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") Long id) {
+        eventDAO.delete(id);
+        return "redirect:/events";
+    }
 }
---
+++ src/main/java/ua/nicety/controllers/ScheduleController.java
@@ -0,0 +1,80 @@
+package ua.nicety.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import ua.nicety.DAO.ScheduleDAO;
+import ua.nicety.model.Schedule;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/sсhedules")
+public class ScheduleController {
+
+    private final ScheduleDAO scheduleDAO;
+
+    @Autowired
+    public ScheduleController(ScheduleDAO scheduleDAO) {
+        this.scheduleDAO = scheduleDAO;
+    }
+
+//  Show all user schedules
+    @GetMapping("/{id}")
+    public String userSchedules(
+            @PathVariable("id") Schedule schedule,
+            Model model) {
+        model.addAttribute("schedules", scheduleDAO.allUserSchedules(schedule.getUserId()));
+
+        return "schedules/show";
+    }
+
+//  Create new Schedule
+    @GetMapping("/new")
+    public String newSchedule(Model model, Schedule schedule) {
+        model.addAttribute("schedule", schedule);
+
+        return "schedules/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("schedule") @Valid Schedule schedule,
+                         BindingResult bindingResult) {
+        if (bindingResult.hasErrors())
+            return "schedules/new";
+
+        scheduleDAO.save(schedule);
+
+        return "redirect:/schedules";
+    }
+
+//  Edit & update schedule
+    @GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable("id") Long id) {
+        model.addAttribute("schedule", scheduleDAO.show(id));
+
+        return "schedules/edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("schedule") @Valid Schedule schedule,
+                         BindingResult bindingResult,
+                         @PathVariable("id") Long id) {
+        if (bindingResult.hasErrors())
+            return "schedules/edit";
+
+        scheduleDAO.update(id, schedule);
+
+        return "redirect:/schedules";
+    }
+
+//  Delete schedule
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") Long id) {
+        scheduleDAO.delete(id);
+        return "redirect:/schedules";
+    }
+}
+
