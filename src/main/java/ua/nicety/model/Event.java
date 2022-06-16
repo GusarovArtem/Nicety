697d6cfedb6c41ff7f0572a6d87c50b199e17da0
GusarovArtem <artemmaksimovich1@gmail.com>
2022-06-16 10:59:40 +0300

    Add getters and setters

---
+++ src/main/java/ua/nicety/model/Event.java
@@ -0,0 +1,82 @@
+package ua.nicety.model;
+
+import lombok.Data;
+
+import java.time.LocalTime;
+
+@Data
+public class Event {
+
+    private Long id;
+    private String name;
+    private String description;
+    private String smiles;
+    private Day day;
+    private LocalTime time;
+    private String color;
+    private Long scheduleId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSmiles() {
+        return smiles;
+    }
+
+    public void setSmiles(String smiles) {
+        this.smiles = smiles;
+    }
+
+    public Day getDay() {
+        return day;
+    }
+
+    public void setDay(Day day) {
+        this.day = day;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Long getScheduleId() {
+        return scheduleId;
+    }
+
+    public void setScheduleId(Long scheduleId) {
+        this.scheduleId = scheduleId;
+    }
+}
