dbe0187e425380441de363e21f44e722de0b2528
GusarovArtem <artemmaksimovich1@gmail.com>
2022-06-17 16:02:38 +0300

    Add validation

--- src/main/java/ua/nicety/model/Schedule.java
+++ src/main/java/ua/nicety/model/Schedule.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -11,6 +13,7 @@ public class Schedule {
 
     private Long id;
 
+    @NotEmpty(message = "Please write the name of the schedule")
     private String name;
 
     private Long userId;
