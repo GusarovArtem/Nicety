d34eda6d810716a899f02983db77682188f22397
GusarovArtem <artemmaksimovich1@gmail.com>
2022-06-23 20:47:54 +0300

    Add event creator popup

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,3 +1,17 @@
-// $(function(){
-//     $('.mainText p').hide(2000).show(2000);
-// });
+let popup = document.getElementById('createEventPopup'),
+    popupToggle = document.getElementById('popupBtn'),
+    popupClose = document.querySelector('.close');
+
+    popupToggle.onclick = function () {
+        popup.style.display="block";
+    };
+
+    popupClose.onclick = function () {
+        popup.style.display="none";
+    };
+
+    window.onclick = function (e) {
+        if(e.target === popup) {
+            popup.style.display="none";
+        }
+    }
