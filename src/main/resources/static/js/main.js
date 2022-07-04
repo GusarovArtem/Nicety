ed0968b57933341c00fadbf6dcc0a12912e94799
GusarovArtem <artemmaksimovich1@gmail.com>
2022-07-04 21:08:08 +0300

    Add Description popup

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -3,3 +3,22 @@ let eventFromBack = document.querySelector('.back');
     eventFromBack.onclick = function () {
         history.back();
     };
+
+//    Show Description
+let popup = document.getElementById('showDescriptionBtn'),
+    popupToggle = document.getElementById('showDescriptionPopup'),
+    popupClose = document.querySelector('.close');
+
+popupToggle.onclick = function () {
+    popup.style.display="block";
+};
+
+popupClose.onclick = function () {
+    popup.style.display="none";
+};
+
+window.onclick = function (e) {
+    if(e.target === popup) {
+        popup.style.display="none";
+    }
+}
