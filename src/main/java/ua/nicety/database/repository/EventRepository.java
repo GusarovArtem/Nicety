a3dcd2ebd39dbf5d8df4f30552edc819b8ae942a
GusarovArtem <artemmaksimovich1@gmail.com>
2022-07-12 19:01:26 +0300

    Minor fixes

--- src/main/java/ua/nicety/database/repository/EventRepository.java
+++ src/main/java/ua/nicety/database/repository/EventRepository.java
@@ -2,11 +2,14 @@ package ua.nicety.database.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import ua.nicety.database.entity.event.BaseEvent;
 import ua.nicety.database.entity.event.Event;
 
 import java.util.List;
 
-public interface EventRepository<E> extends JpaRepository<E, Long>  {
+@Repository
+public interface EventRepository<E extends BaseEvent> extends JpaRepository<E, Long>  {
 
     @Query(nativeQuery = true, value = "SELECT * FROM E e where e.schedule_id = :scheduleId")
     List<E> findByScheduleId(String scheduleId);
